db5e410be59826c1d954d6dec3b6b4f05239212a
nsmeds <niilosmeds@gmail.com>
2017-02-07 16:52:22 -0800

    returning post data

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  renderTemplate() {
-    this.render('authors');
-  }
+    // renderTemplate() {
+    //   this.render('authors');
+    // },
+    model() {
+      return this.get('store').findAll('post');
+    }
 });
